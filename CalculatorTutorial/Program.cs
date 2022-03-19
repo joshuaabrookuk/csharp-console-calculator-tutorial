ae6544379a6e2da632e785c5282f7e0bbee64ec4
joshuaabrookuk <joshuabrookuk@gmail.com>
2022-03-19 10:50:21 +0000

    Create a Console Calculator in C# and the .NET framework, include lines to prompt the user for two numbers and an operrator

---
+++ CalculatorTutorial/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CalculatorTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0; int num2 = 0;
+
+            Console.WriteLine("Console Calculator in C#\r");
+            Console.WriteLine("------------------------\r");
+
+            Console.WriteLine("Type a whole number, and then press Enter");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Type another whole number, and then press Enter");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Choose an operator from the following list:");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Subtract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.Write("Type the key of your chosen function, and then press Enter? ");
+
+            switch (Console.ReadLine())
+            {
+                case "a":
+                    Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+                    break;
+                case "s":
+                    Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+                    break;
+                case "m":
+                    Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+                    break;
+                case "d":
+                    Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+                    break;
+            }
+
+            Console.Write("Press any key to close the Calculator console app");
+            Console.ReadKey();
+        }
+    }
+}
