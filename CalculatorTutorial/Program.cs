e932191ce5d87f9e9490017861e173a89e6929c7
joshuaabrookuk <joshuabrookuk@gmail.com>
2022-03-25 09:49:09 +0000

    Add decimal functionality

--- CalculatorTutorial/Program.cs
+++ CalculatorTutorial/Program.cs
@@ -6,16 +6,16 @@ namespace CalculatorTutorial
     {
         static void Main(string[] args)
         {
-            int num1 = 0; int num2 = 0;
+            double num1 = 0; double num2 = 0;
 
             Console.WriteLine("Console Calculator in C#\r");
             Console.WriteLine("------------------------\r");
 
-            Console.WriteLine("Type a whole number, and then press Enter");
-            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type a number, and then press Enter");
+            num1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Type another whole number, and then press Enter");
-            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type another number, and then press Enter");
+            num2 = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine("Choose an operator from the following list:");
             Console.WriteLine("\ta - Add");
--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 [Tutorial: Create a simple C# console app in Visual Studio](https://docs.microsoft.com/en-us/visualstudio/get-started/csharp/tutorial-console?view=vs-2022)
 
 - [x] Add code to create a calculator
+- [x] Add decimal functionality
+- [ ] Debug the app
 
 
 Curent Teminal Demonstration
@@ -12,9 +14,9 @@ Curent Teminal Demonstration
 
 `Console Calculator in C#` <br>
 `------------------------` <br>
-`Type a whole number, and then press Enter` <br>
+`Type a number, and then press Enter` <br>
 `5` <br>
-`Type another whole number, and then press Enter` <br>
+`Type another number, and then press Enter` <br>
 `5` <br>
 `Choose an operator from the following list:` <br>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`a - Add` <br>
